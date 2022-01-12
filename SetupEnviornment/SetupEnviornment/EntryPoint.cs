999047c88d3d245f8f0ff0ea5b165cab1b95cd70
VanMorbir <vanmorbir@gmail.com>
2022-01-12 13:31:35 +0200

    added element test

--- SetupEnviornment/SetupEnviornment/EntryPoint.cs
+++ SetupEnviornment/SetupEnviornment/EntryPoint.cs
@@ -7,8 +7,37 @@ class EntryPoint
     {
         IWebDriver driver = new ChromeDriver();
 
+        //initial test
         driver.Navigate().GoToUrl("http://testing.todorvachev.com");
         Thread.Sleep(3000);
         driver.Quit();
+
+        //element test
+        driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("http://testing.todorvachev.com/name/");
+        IWebElement element = driver.FindElement(By.Name("myName"));
+        if (element.Displayed)
+        {
+            GreenMessage("element is visible");
+        }
+        else
+        {
+            RedMessage("element is NOT VISIBLE");
+        }
+        driver.Quit();
+    }
+
+    private static void RedMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
     }
 }

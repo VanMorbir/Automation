ad65062116f4775b1767518112bc5690221b9cb0
VanMorbir <vanmorbir@gmail.com>
2022-01-12 12:58:44 +0200

    added initial test

--- SetupEnviornment/SetupEnviornment/EntryPoint.cs
+++ SetupEnviornment/SetupEnviornment/EntryPoint.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class EntryPoint
+{
+    public static void Main()
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("http://testing.todorvachev.com");
+        Thread.Sleep(3000);
+        driver.Quit();
+    }
+}

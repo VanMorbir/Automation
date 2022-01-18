18ef76449321cd4210e538929b50e20b58c10cd3
VanMorbir <vanmorbir@gmail.com>
2022-01-18 13:51:54 +0200

    added requestSpecs

--- RestProject/src/test/java/TestConfig.java
+++ RestProject/src/test/java/TestConfig.java
@@ -1,12 +1,20 @@
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.junit.BeforeClass;
 
 public class TestConfig {
     @BeforeClass
     public static void setup(){
-        RestAssured.proxy("localhost", 8888);
+        //RestAssured.proxy("localhost", 8888);
         RestAssured.baseURI = "http://167.99.197.177";
         RestAssured.basePath = "/app/";
         RestAssured.port = 8080;
+
+        RequestSpecification requestSpec = new RequestSpecBuilder()
+                .addHeader("Content-Type","application/json")
+                .addHeader("Accept", "application/json")
+                .build();
+        RestAssured.requestSpecification = requestSpec;
     }
 }

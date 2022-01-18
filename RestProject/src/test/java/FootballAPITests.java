e640880d2a94046e775496966046faebcfd76c53
VanMorbir <vanmorbir@gmail.com>
2022-01-18 16:56:43 +0200

    added tests, fixed static setup()

--- RestProject/src/test/java/FootballAPITests.java
+++ RestProject/src/test/java/FootballAPITests.java
@@ -1,4 +1,26 @@
 import config.FootballAPIConfig;
+import org.junit.Test;
+import static org.hamcrest.Matchers.equalTo;
+
+import static io.restassured.RestAssured.*;
 
 public class FootballAPITests extends FootballAPIConfig {
+
+    @Test
+    public void getDetailsOfOneArea(){
+        given()
+                .queryParam("areas", 2072).
+        when()
+                .get("areas").
+        then();
+    }
+
+    @Test
+    public void getDateFounded(){
+        given().
+        when().
+                get("teams/57").
+        then().
+                body("founded", equalTo(1886));
+    }
 }
--- RestProject/src/test/java/config/FootballAPIConfig.java
+++ RestProject/src/test/java/config/FootballAPIConfig.java
@@ -3,6 +3,8 @@ package config;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.junit.BeforeClass;
@@ -12,12 +14,14 @@ public class FootballAPIConfig {
     public static ResponseSpecification responseSpec;
 
     @BeforeClass
-    public void setup(){
+    public static void setup(){
         football_requestSpec = new RequestSpecBuilder()
                 .setBaseUri("http://api.football-data.org")
                 .setBasePath("/v2/")
                 .addHeader("X-Auth-Token", "3d7a7a6046b1495e9db894ef08493d72")
                 .addHeader("X-Response-Control", "minified")
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
                 .build();
         responseSpec = new ResponseSpecBuilder()
                 .expectStatusCode(200)

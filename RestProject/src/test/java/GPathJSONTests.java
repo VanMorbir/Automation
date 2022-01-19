31d84e226022ff66e24e98541cf6f4ec91c5dff8
VanMorbir <vanmorbir@gmail.com>
2022-01-19 14:03:21 +0200

    Update GPathJSONTests.java

--- RestProject/src/test/java/GPathJSONTests.java
+++ RestProject/src/test/java/GPathJSONTests.java
@@ -1,5 +1,6 @@
 import config.FootballAPIConfig;
 import io.restassured.response.Response;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Map;
@@ -14,5 +15,14 @@ public class GPathJSONTests extends FootballAPIConfig {
         Map<String,?> allDataForSingleTeam = response.path(
                 "teams.find { it.name == 'Manchester United FC'}");
         System.out.println("Map of Team Data: " + allDataForSingleTeam);
+    }
+
+    @Test
+    public void extractSingleValueWithFind(){
+        Response response = get("teams/57");
+        String singlePlayer = response.path(
+                "squad.find { it.shirtNumber == 48}.name"
+        );
+        Assert.assertEquals("Albert Sambi Lokonga", singlePlayer);
     }
 }

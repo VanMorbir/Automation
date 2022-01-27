09de7f3f14dd257394ada1f38c19d8bcc1148c1b
GilGetreuer <vanmorbir@gmail.com>
2022-01-27 14:43:17 +0200

    added appium

--- ParaBankTesting/src/test/java/config/uiconfig/ParaBankActions.java
+++ ParaBankTesting/src/test/java/config/uiconfig/ParaBankActions.java
@@ -1,16 +1,34 @@
 package config.uiconfig;
 import config.uiconfig.uielements.ContactPage;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.time.Duration;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class ParaBankActions {
     public static WebDriver InitDriver(){
         System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.navigate().to(ParaBankUIConfig.baseURL);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        return driver;
+    }
+
+    public static AndroidDriver<WebElement> InitMobileDriver() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, ParaBankUIConfig.deviceName);
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+        capabilities.setCapability("chromedriverExecutable", "C:\\Users\\Gil\\Documents\\GitHub\\Automation\\src\\main\\resources\\chromedriver.exe");
+
+        AndroidDriver<WebElement> driver = new AndroidDriver<>(new URL(ParaBankUIConfig.appiumServer), capabilities);
+        driver.navigate().to(ParaBankUIConfig.baseURL);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         return driver;
     }
 
